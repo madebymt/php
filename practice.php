b79fc66808694b5afa9ea0fd5dff9fdaa749e313
Ruby <c19890611@gmail.com>
2017-12-11 23:30:16 -0600

    start new php

---
+++ practice.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>My First PHP Web Page</title>
+	</head>
+	<body>
+	<?php print('My name is Ruby'); ?>
+	<?php print('Hello World!'); ?>
+	<?php
+	$my_name = 'Ruby';
+	$my_age= 27;
+	$my_home = 'Houston'
+	?>
+
+	<?php
+
+print("my name is $my_name! <br>
+I am $my_age years old, and my home is $my_home."); ?>
+
+<?php
+define("TITLE","Defining Constants")
+?>
+
+<?php
+echo TITLE
+?>
+
+<?php ?>
+<?php ?>
+<?php ?>
+<?php ?>
+<?php ?>
+<?php ?>
+
+
+
+
+	</body>
+</html>
